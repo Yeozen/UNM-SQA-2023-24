d00b8e08211d7778ab93cdd09dd5f49f60bfab00
Yeozen <88418323+Yeozen@users.noreply.github.com>
2023-10-24 15:39:29 +0800

    Update youtubeapi.js

--- youtubeapi.js
+++ youtubeapi.js
@@ -1,10 +1,7 @@
-// Set your YouTube Data API key
-var apiKey = "AIzaSyASw0iFX4bRcwz10Lmm12CdTgxF-NLzve4";
-
 var apiKey = "AIzaSyASw0iFX4bRcwz10Lmm12CdTgxF-NLzve4"; // Replace with your YouTube Data API key
 var maxResults = 12;
 var videoDuration = "short";
-var searchQuery = "car"; // Replace with your search query
+var searchQuery = "software quality assurance"; // Replace with your search query
 
 fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${searchQuery}&type=video&videoDuration=${videoDuration}&key=${apiKey}`)
   .then(response => response.json())
@@ -12,7 +9,7 @@ fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${ma
     var videoListDiv = document.getElementById("videoList");
     var videoPlayer = document.getElementById("videoPlayer");
 
-    data.items.forEach(function(item) {
+    data.items.forEach(function(item, index) {
       var title = item.snippet.title;
       var videoId = item.id.videoId;
       var videoThumbnail = item.snippet.thumbnails.medium.url;
@@ -20,7 +17,7 @@ fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${ma
       var videoElement = document.createElement("div");
       videoElement.className = "video-box";
       videoElement.addEventListener("click", function() {
-        videoPlayer.src = "https://www.youtube.com/embed/" + videoId;
+        openVideoPlayer(videoId);
       });
 
       var thumbnailImage = document.createElement("img");
@@ -33,13 +30,18 @@ fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${ma
       videoElement.appendChild(thumbnailImage);
       videoElement.appendChild(titleParagraph);
       videoListDiv.appendChild(videoElement);
+
+      // Load the first video in the iframe upon page load
+      if (index === 0) {
+        openVideoPlayer(videoId);
+      }
     });
   })
   .catch(error => {
     console.error("Error fetching data:", error);
   });
 
-  
-  
-  
-  
+function openVideoPlayer(videoId) {
+  var videoPlayer = document.getElementById("videoPlayer");
+  videoPlayer.src = "https://www.youtube.com/embed/" + videoId;
+}
